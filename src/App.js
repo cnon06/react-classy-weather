a10d9067f53c2b658f93d5758878b6cd4dc1d1a4
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-10-10 07:52:30 +0300

    33

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    // console.log(this);
+    this.setState((curstate) => {
+      return { count: curstate.count - 1 };
+    });
+  }
+  handleIncrement() {
+    // console.log(this);
+    this.setState((curstate) => {
+      return { count: curstate.count + 1 };
+    });
+  }
+
+  render() {
+    const date = new Date("June 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]{" "}
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div>app</div>
+//   );
+// }
+
+// export default App;
